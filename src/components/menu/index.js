ecfa9c4913d09a6ed7ce3300d3ff677666611a07
Eivind Hjertnes <me@hjertnes.me>
2019-08-08 05:49:15 +0200

    fix: remove old no longer existing imports

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import Authenticated from './authenticated';
 import Public from './public';
 
 const Menu = () => <Public />;
